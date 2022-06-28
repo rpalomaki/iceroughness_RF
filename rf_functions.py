cbeb7645c48a88a9dcf58da781430562be3d9bf9
Ross Palomaki <ross.palomaki@gmail.com>
2022-06-28 17:22:44 -0600

    finish data_setup func

--- rf_functions.py
+++ rf_functions.py
@@ -2,19 +2,38 @@ import pandas as pd
 
 
 
-def data_setup(s1_fp, s1_units='db', drop_vv_glcm=True):
+def data_setup(s1_fp, stats_fp, moran_fp, date=None, s1_units='dB', drop_vv_glcm=True,
+               drop_ad=True):
     """
+    Function to compile all input data to run icer roughness RF models.
 
     Arguments
     ---------
     s1_fp : str
-        stuff
-    s1_units : 'db' or 'raw_power'
-        stuff
-    drop_vv_glcm : bool (default True)
-        stuff
-    
-    
+        Filepath to Sentinel 1 data.
+    stats_fp : str
+        Filepath to statistics data.
+    moran_fp : str
+        Filepath to Moran's I data.
+    date : str, default None
+        A date used to subset the larger dataset. If None, returns all 
+        available data.
+    s1_units : 'dB' (default) or 'raw_power'
+        If 'dB', converts raw VV and VH Sentinel 1 returns to dB scale.
+    drop_vv_glcm : bool, default True
+        Controls whether or not to drop VV GLCM data (currently, VV GLCM does 
+        not produce meaningful results).
+    drop_ad : bool, default True
+        Controls whether or not to drop Anderson-Darling test data from the 
+        statistics dataset. Currently AD test does not produce meaningful 
+        results.
+
+    Returns
+    -------
+    targets : pd.Dataframe
+        DataFrame containing the possible RF targets.
+    predictors : pd.DataFrame
+        DataFrame containing the possible RF predictors.
     """
     # Read/tidy S1 data
     s1_data = pd.read_csv(s1_fp)
@@ -45,3 +64,30 @@ def data_setup(s1_fp, s1_units='db', drop_vv_glcm=True):
     # Create derived metrics - targets
     targets['iqr'] = targets['roughness_p75'] - targets['roughness_p25']
     targets['p95-p5'] = targets['roughness_p95'] - targets['roughness_p5']
+
+    # Add lognorm fit parameters into targets
+    stats_data = pd.read_csv(stats_fp)
+    stats_data['date'] = stats_data['date'].astype(str).str.zfill(4)
+    stats_data.index = pd.MultiIndex.from_arrays([stats_data['date'], stats_data['S1_pixel_ID']])
+    stats_data.drop(columns=['date','S1_pixel_ID'], inplace=True)
+    if drop_ad:
+        stats_data.drop(columns=['ad'], inplace=True)
+    targets = targets.join(stats_data)
+
+    # Add moran data into targets
+    moran = pd.read_csv(moran_fp)
+    moran['date'] = moran['date'].astype(str).str.zfill(4)
+    moran.index = pd.MultiIndex.from_arrays([moran['date'], moran['S1_pixel_ID']])
+    moran.drop(columns=['date','S1_pixel_ID'], inplace=True)
+    moran.columns = ['moran','moran_z','moran_p']
+    targets = targets.join(moran)
+
+    # Subset by date if necessary
+    if date == '0218':
+        targets = targets.loc['0218']
+        predictors = predictors.loc['0218']
+    elif date == '0302':
+        targets = targets.loc['0302']
+        predictors = predictors.loc['0302']
+    
+    return targets, predictors
