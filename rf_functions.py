af5406b9b37d18944d678709f2ce32d4ca2b41c1
Ross Palomaki <ross.palomaki@gmail.com>
2022-07-18 10:59:58 -0600

    func testing

--- rf_functions.py
+++ rf_functions.py
@@ -2,7 +2,8 @@ import pandas as pd
 import numpy as np
 import re
 from datetime import datetime
-from sklearn.ensemble import RandomForestRegressor
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
 from tqdm.notebook import tqdm
@@ -104,10 +105,9 @@ def data_setup(s1_fp, stats_fp, moran_fp, date=None, s1_units='dB', drop_vv_glcm
     return targets, predictors
 
 
-def run_rf(targets, predictors, n_runs=100, rf_type='single_target', 
-           train_frac=0.7, rf_params=None, random_state=5033, 
-           output_dir_predict=None, output_dir_metrics=None, 
-           out_file_prefix=None, return_vals=False):
+def run_rf_reg(targets, predictors, n_runs=100, rf_type='single_target', 
+    train_frac=0.7, rf_params=None, random_state=5033, output_dir_predict=None, 
+    output_dir_metrics=None, out_file_prefix=None, return_vals=False):
     """
     
     Arguments
@@ -236,19 +236,146 @@ def run_rf(targets, predictors, n_runs=100, rf_type='single_target',
 
 
 
-# def plot_results():
-#     """
+def confusion_heatmap(cm, ax, cmap, vmin=0, vmax=1, cbar_label=None,
+    xlabel=True, ylabel=True, fig_title=None):
+    """
     
-#     """
-#     plt.close('all')
-#     fig, ax = plt.subplots()
-#     hex1 = ax.hexbin(predict_list, valid_list, gridsize=50, cmap='inferno')
-#     ax.plot((min(predict_list), max(predict_list)), (min(predict_list), max(predict_list)), 'r--')
-#     cb1 = fig.colorbar(hex1, ax=ax)
-#     cb1.set_label(f'Counts ({len(predict_list)} total predictions)', labelpad=12)
-#     ax.set_xlabel('Predicted')
-#     ax.set_ylabel('Measured', labelpad=12)
-#     ax.set_title(f'{target_col} of 10m pixels trained on both dates')
-#     plt.tight_layout()
-#     if output_figures:
-#         fig.savefig(hex_figure_fname, dpi=300)
+    """
+    im = ax.imshow(cm, cmap=cmap, vmin=vmin, vmax=vmax)
+    ax.set_xticks(np.arange(cm.shape[1]+1)-.5, minor=True)
+    ax.set_yticks(np.arange(cm.shape[0]+1)-.5, minor=True)
+    ax.grid(which="minor", color="w", linestyle='-', linewidth=3)
+    ax.tick_params(axis='both', which='minor', bottom=False, left=False)
+    ax.set_xticks(ax.get_xticks()[1:-1])
+    ax.set_yticks(ax.get_yticks()[1:-1])
+    # ax.set_xticklabels(cols, fontsize=16)
+    # ax.set_yticklabels(ind, fontsize=16)
+    cbar_ratio = cm.shape[0]/cm.shape[1]
+    cbar = plt.colorbar(im, ax=ax[j], fraction=0.046*cbar_ratio, pad=0.04)
+    cbar.ax.tick_params(labelsize=18)
+    if cbar_label: 
+        cbar.ax.set_ylabel(cbar_label, rotation=-90, 
+                            va='bottom', fontsize=20, labelpad=12)
+        
+
+    for i in range(len(cm)):
+        for k in range(len(cm)): # cm guaranteed to be square
+            text = ax[j].text(k, i, cm.iloc[i, k], fontsize=16,
+                        ha='center', va='center', color='k')
+
+    ax.spines['bottom'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+
+    if xlabel: 
+        ax.set_xlabel('Predicted label', fontsize=20, labelpad=14)
+    if ylabel: 
+        ax.set_ylabel('True label', fontsize=20, labelpad=14)
+    if fig_title:
+        ax.set_title(fig_title, fontsize=20)
+    
+
+
+
+def run_rf_cla(targets, predictors, rf_params=None, n_runs=100, train_frac=0.7, n_classes=5, 
+    class_split_method='percentile', plot_cm =True, 
+    random_state=5033, output_dir_predict=None, 
+    output_dir_cm=None, out_file_prefix=None, return_vals=False):
+    """
+    stuff
+    """
+# Some initial setup
+    target_count = 1
+    for target_col in targets.columns:
+        print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                f'-- Starting target {target_col} ({target_count}/{len(targets.columns)} targets)')
+        target_count += 1
+        # Create classes based on n_classes
+        X = predictors
+        target_raw = targets[target_col]
+        if class_split_method == 'percentile':
+            p_bounds = np.linspace(0, 100, n_classes+1)
+            percentiles = [np.percentile(target_raw, p) for p in p_bounds]
+            class_labels = np.arange(1, len(p_bounds))
+        else:
+            # Reserve for other split methods methods
+            raise ValueError("class_split_method must be 'percentile'.")
+        y = pd.cut(target_raw, bins=percentiles, labels=class_labels)
+        # NaN check
+        nan_ind = y.loc[pd.isnull(y)].index
+        y.drop(index=nan_ind, inplace=True)
+        X.drop(index=nan_ind, inplace=True)
+        
+        valid_list, predict_list, n_run_list = [], [], []
+        # If more than one date present, use weighted sampling for t/t split
+        # First, create index check object
+        ind_check = np.unique(np.array([x[0] for x in targets.index]))
+        if len(ind_check) > 1:
+            nsamples_0218 = int(targets.loc['0218'].shape[0]*train_frac)
+            nsamples_0302 = int(targets.loc['0302'].shape[0]*train_frac)
+        
+        # Initiate RandomForestClassifier
+        if rf_params is None:
+            # Default parameters
+            ntrees = 2000    # 2000 recommended in 10.5194/hess-25-2997-2021
+            mtry = 2/3       # From paper above; passed to 'max_features' kwarg in RF regressor
+            max_depth = 7    # Max depth of each tree
+            rf = RandomForestClassifier(n_estimators=ntrees,
+                            max_features=mtry,
+                            max_depth=max_depth, 
+                            random_state=random_state)
+        else:
+            try:
+                rf = RandomForestClassifier(**rf_params)
+            except TypeError:
+                print('rf_params must be a dict with valid arguments \
+                        for the sklearn RandomForestClassifier class.')
+                return None
+            
+        
+        # Loop of RF runs
+        for i in tqdm(range(n_runs)):
+            # Train/test split
+            if len(ind_check) > 1:
+                # Weighted sampling of multiple dates
+                train_ind_0218 = pd.MultiIndex.from_product([['0218'], targets.loc['0218'].sample(nsamples_0218).index])
+                train_ind_0302 = pd.MultiIndex.from_product([['0302'], targets.loc['0302'].sample(nsamples_0302).index])
+                train_ind = train_ind_0218.union(train_ind_0302)
+                test_ind = targets[~targets.index.isin(train_ind)].index
+                X_train, X_test = X.loc[train_ind], X.loc[test_ind]
+                y_train, y_test = y.loc[train_ind], y.loc[test_ind]
+            else:
+                # Single date only
+                X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=train_frac)
+            # Fit and predict
+            rf.fit(X_train, y_train)
+            predictions = rf.predict(X_test)
+            # Save values to list
+            valid_list.extend(y_test.values.tolist())
+            predict_list.extend(predictions)
+            n_run_list.extend(np.repeat(int(i+1), len(predictions)))
+
+        predict_df = pd.DataFrame(np.array([n_run_list, predict_list, valid_list]).T, 
+                                    columns=['run_no','predict','valid'])
+        
+        if output_dir_predict:
+            if 'single' in output_dir_predict:
+                date = targets.index[0][0]
+                if date == '0218':
+                    date = '0219'
+                fp_p = f'{output_dir_predict}{date}/{out_file_prefix}_{target_col}.csv'
+            else:
+                fp_p = f'{output_dir_predict}{out_file_prefix}_{target_col}.csv'
+            predict_df.to_csv(fp_p)
+
+        if plot_cm:
+            # Confusion matrix
+            cm = metrics.confusion_matrix(y_true=valid_list, y_pred=predict_list)
+            fig, ax = plt.subplots(figsize=(6,6))
+            confusion_heatmap(cm, ax, cmap='Blues', vmin=0, vmax=cm.max(),
+                              cbar_label='Count', fig_title=target_col)
+            fig.savefig(f'{output_dir_cm}/{target_col}.png', facecolor='white',
+                        dpi=300)
+            
+        
